969ea56ff3b26feb88c4740b5a5bf91cffdf0f6a
cheweilab <92781763+cheweilab@users.noreply.github.com>
2024-07-26 20:58:36 +0800

    Update app.js
    Return the input field to blankl after input for better user experience.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var main = function () {
             comment_text = $(".comment-input input").val();
             $new_comment.text(comment_text);
         $(".comments").append($new_comment);
+        comment_text = $(".comment-input input").val("");
     });
 };
 $(document).ready(main);
