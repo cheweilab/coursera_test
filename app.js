99bb271efda4f2b15f620284f5a081ba7f5df307
Your Name <kanahei.hsieh@gmail.com>
2024-07-26 20:36:45 +0800

    Initial commit of a simple comment app

---
+++ app.js
@@ -0,0 +1,10 @@
+var main = function () {
+    "use strict";
+    $(".comment-input button").on("click", function () {
+        var $new_comment = $("<p>"),
+            comment_text = $(".comment-input input").val();
+            $new_comment.text(comment_text);
+        $(".comments").append($new_comment);
+    });
+};
+$(document).ready(main);
