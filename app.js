5a4b6b7fa5e9b2c02d2b8d7d251e6f3dc9950dd4
Your Name <kanahei.hsieh@gmail.com>
2024-07-27 10:07:48 +0800

    Merge remote-tracking branch 'origin/main' into main#   (use "git pull" to update your local branch)

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
 var main = function () {
     "use strict";
+
+    /*click event*/
+    
     $(".comment-input button").on("click", function () {
         var $new_comment = $("<p>"),
             comment_text = $(".comment-input input").val();
             $new_comment.text(comment_text);
         $(".comments").append($new_comment);
         comment_text = $(".comment-input input").val("");
+        if (comment_text = $(".comment-input input").val()!==""){
+
+            $(".comments").append($new_comment);
+        comment_text = $(".comment-input input").val("");
+
+        }
+        
     });
+
+    /* enter keypress */
+
+    $(".comment-input input").on("keypress", function (event) {
+        if (event.keyCode === 13) {  
+            var $new_comment = $("<p>"),
+                comment_text = $(".comment-input input").val();
+            
+            $new_comment.text(comment_text);
+            
+            if (comment_text !== "") {  
+                $(".comments").append($new_comment); 
+                $(".comment-input input").val("");
+            }
+        }
+    });
+    
+
+
 };
 $(document).ready(main);
