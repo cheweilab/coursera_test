020c42274e072ea845709132243dbd2cd5cd40e2
Your Name <kanahei.hsieh@gmail.com>
2024-07-27 11:56:27 +0800

    $(".comment-input input").on("keypress", function (event) {     if (event.keyCode === 13) {         var $new_comment = $("<p>"),         $checkbox = $('<input>').attr({             type: 'checkbox',             name: 'someCheckbox',             value: 'someValue'         }),         comment_text = $(".comment-input input").val();
            if (comment_text !== "") {
                $new_comment.append($checkbox);
                $new_comment.append(document.createTextNode(comment_text));
                $(".comments").append($new_comment);
                $(".comment-input input").val("");
            }
        }
    }); // adds a checkbox type to the comments section

--- app.js
+++ app.js
@@ -2,38 +2,59 @@ var main = function () {
     "use strict";
 
     /*click event*/
-    
+
     $(".comment-input button").on("click", function () {
+
         var $new_comment = $("<p>"),
+            $checkbox = $('<input>').attr({
+                type: 'checkbox',
+                name: 'someCheckbox',
+                value: 'someValue'
+            }),
             comment_text = $(".comment-input input").val();
-            $new_comment.text(comment_text);
-        $(".comments").append($new_comment);
-        comment_text = $(".comment-input input").val("");
-        if (comment_text = $(".comment-input input").val()!==""){
-
+    
+      
+        if (comment_text !== "") {
+          
+            $new_comment.append($checkbox);
+    
+      
+            $new_comment.append(document.createTextNode(comment_text));
+    
+           
             $(".comments").append($new_comment);
-        comment_text = $(".comment-input input").val("");
-
+    
+            $(".comment-input input").val("");
         }
-        
     });
+    
+    
+    
+
+
+
 
     /* enter keypress */
-
     $(".comment-input input").on("keypress", function (event) {
-        if (event.keyCode === 13) {  
+        if (event.keyCode === 13) {
             var $new_comment = $("<p>"),
-                comment_text = $(".comment-input input").val();
-            
-            $new_comment.text(comment_text);
-            
-            if (comment_text !== "") {  
-                $(".comments").append($new_comment); 
+            $checkbox = $('<input>').attr({
+                type: 'checkbox',
+                name: 'someCheckbox',
+                value: 'someValue'
+            }),
+            comment_text = $(".comment-input input").val();
+    
+            if (comment_text !== "") {
+                $new_comment.append($checkbox);
+                $new_comment.append(document.createTextNode(comment_text));
+                $(".comments").append($new_comment);
                 $(".comment-input input").val("");
             }
         }
-    });
+    }); 
     
+
 
 
 };
