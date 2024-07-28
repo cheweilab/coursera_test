053747bb5e17edf5427df6fdb6cb66e2cbf6a2c4
Your Name <kanahei.hsieh@gmail.com>
2024-07-28 11:53:07 +0800

    Modification: 1. jquery events 2. without UX

--- app.js
+++ app.js
@@ -1,50 +1,74 @@
 var main = function () {
     "use strict";
 
+    /*Objective:  Task交接本
+    1. create A delete button that will remove all checked notes
+    2. Advanced: 
+    (1) Commentary creators will be shown with a thumbnail after the added comment
+    */
+
     /*click event*/
 
     $(".comment-input button").on("click", function () {
-
         var $new_comment = $("<p>"),
             $checkbox = $('<input>').attr({
                 type: 'checkbox',
                 name: 'someCheckbox',
                 value: 'someValue'
             }),
+            $newImage = $("<img>").attr({
+                src: 'https://w7.pngwing.com/pngs/625/201/png-transparent-shock-emoji-emoji-iphone-computer-icons-emoji-head-smiley-sticker-thumbnail.png',
+                alt: 'Shock emoji',
+                style: 'width: 20px; height: 20px; margin-left: 5px;'
+            }),
             comment_text = $(".comment-input input").val();
-    
-      
+
         if (comment_text !== "") {
-          
             $new_comment.append($checkbox);
-    
-      
+            $new_comment.append(" "); // Add a space for readability
             $new_comment.append(document.createTextNode(comment_text));
-    
-           
+            $new_comment.append($newImage);
+
             $(".comments").append($new_comment);
-    
+
             $(".comment-input input").val("");
+
+           
+            $checkbox.on('change', function () {
+                if (this.checked) {
+                    $(this).closest('p').css('text-decoration', 'line-through');
+                } else {
+                    $(this).closest('p').css('text-decoration', 'none');
+                }
+            });
         }
     });
-    
-    
-    
+
+    /* minus click event */
+
+    $(".functions button").on("click", function () {
+        console.log("hello");
+
+        if ($(".comments input:checked").length > 0) {
+            $(".comments input:checked").closest("p").remove();
+        }
+      
 
 
 
+    });
 
     /* enter keypress */
     $(".comment-input input").on("keypress", function (event) {
         if (event.keyCode === 13) {
             var $new_comment = $("<p>"),
-            $checkbox = $('<input>').attr({
-                type: 'checkbox',
-                name: 'someCheckbox',
-                value: 'someValue'
-            }),
-            comment_text = $(".comment-input input").val();
-    
+                $checkbox = $('<input>').attr({
+                    type: 'checkbox',
+                    name: 'someCheckbox',
+                    value: 'someValue'
+                }),
+                comment_text = $(".comment-input input").val();
+
             if (comment_text !== "") {
                 $new_comment.append($checkbox);
                 $new_comment.append(document.createTextNode(comment_text));
@@ -52,10 +76,8 @@ var main = function () {
                 $(".comment-input input").val("");
             }
         }
-    }); 
-    
+    });
 
+}; 
 
-
-};
 $(document).ready(main);
